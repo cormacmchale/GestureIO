d3103a8802cec597031fdcdd317dd25a72b28d50
Kevin Niland <G00342279@gmit.ie>
2020-01-22 14:56:33 +0000

    Able to save frame of video

--- webcam.py
+++ webcam.py
@@ -1,13 +1,11 @@
 import cv2, time
+import numpy as np
 
 webcam = cv2.VideoCapture(0)
 
 a = 0
-running = True
 
-while (running):
-    a += 1
-
+while True:
     check, frame = webcam.read()
 
     print(check)
@@ -22,9 +20,11 @@ while (running):
 
     if key == ord('q'):
         running = False
-        break
 
-print(a)
+        # Save numpy array to text file
+        np.savetxt("test.txt", frame.reshape((3,-1)), fmt="%s", header=str(frame.shape))
+        # numpy.save("nxx.npy", frame)
+        break
 
 webcam.release()
 
