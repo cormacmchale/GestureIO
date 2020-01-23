60ed867f3f6ce3c41d23c29fe79cb11ee14deca8
Kevin Niland <G00342279@gmit.ie>
2020-01-23 16:58:01 +0000

    Minor changes - folders for dataset and comments

--- accessOSK.py
+++
@@ -1,4 +0,0 @@
-import keyboard
-
-keyboard.write("Hello world!")
-
--- webcam.py
+++ webcam.py
@@ -1,9 +1,9 @@
-import cv2, time
-import numpy as np
+import cv2
 import keyboard
 from matplotlib import pyplot as plt
-from PIL import Image  
+import numpy as np
 import PIL
+from PIL import Image  
 
 # Remove black bars from video
 webcam = cv2.VideoCapture(cv2.CAP_DSHOW)
@@ -19,38 +19,61 @@ endRight = 300
 endBottom = 300
 color = (0, 255, 0)
 thickness = 1
+
 counter = 1
+
 while (running):
     check, frame = webcam.read()
-    # Use Canny to find edges in the image
+
+    # cv2.rectangle() method is used to draw a rectangle on any image - gesture will be performed within the 
+    # rectangle - For proccessing the image, the dataset will be built from the image captured within the rectangle
     newFrame = cv2.rectangle(frame, (startLeft, startTop), (endRight, endBottom), color, thickness)
+
+    # Use Canny to find edges in the image
     edges = cv2.Canny(frame, firstThreshold, secondThreshold)
     # edgesTwo = frame[80:80,300:300]
-    cv2.imshow("Capturing...", edges)
+
+    cv2.imshow("SignWriter", edges)
     key = cv2.waitKey(1)
 
     # If user presses 'Q', program will will quit
     if key == ord('q'):
-        cv2.imwrite(str(counter)+'originalImage.png', cv2.Canny(frame, 100, 100))
+        # Save frame of webcam to an image
+        cv2.imwrite('temp/originalImage.png', cv2.Canny(frame, 100, 100))
+
         # Open the captured gesture
-        img = Image.open(str(counter)+'originalImage.png')
+        # No need to create multiple original images as the dataset won't be built from this image
+        img = Image.open('temp/originalImage.png') 
         img = img.crop((startLeft, startTop, endRight, endBottom))
-        img.save(str(counter)+'croppedImage.png')
+        
+        img.save('dataset/images/croppedImage' + str(counter) + '.png')
         img.load()
-        data = np.asarray(img, dtype="int32" )
-        print(data.size)
-        #running = False
-        np.savetxt(str(counter)+'imagedata.txt', data,fmt='%1.0f')
-        counter=counter+1        
-        # Save numpy array to text file
+
+        data = np.asarray(img, dtype='int32')
+        # print(data.size)
+
+        # running = False
+
+        np.savetxt('dataset/data/imagedata' + str(counter) + '.txt', data, fmt='%1.0f')
+        counter += 1   
+
+# Stop the webcam
+webcam.release()
+
+cv2.destroyAllWindows
+
+''' Unused code '''
+''' =========== '''
+# Save numpy array to text file
         # np.savetxt("imagedata.txt", edges.reshape((3, -1)), fmt="%s", header=str(frame.shape)) # With header
 
-        #pixelInfo = cv2.imread('croppedImage.png')
+        # pixelInfo = cv2.imread('croppedImage.png')
         # print(pixelinfo.size)
 
-        #pixelInfo[pixelInfo>0] = 255
-        #pixelInfo[pixelInfo==0] = 0
-                # Unused as for now - revisit when building the dataset
+        # pixelInfo[pixelInfo>0] = 255
+        # pixelInfo[pixelInfo==0] = 0
+
+        # Unused as for now - revisit when building the dataset
         # wPercent = (basewidth / float(img.size[0]))
         # hSize = int((float(img.size[1]) * float(wPercent)))
 
@@ -60,15 +83,11 @@ while (running):
         # edgesCheck = newEdges.thumbnail(100,Image.ANTIALIAS)
         # cv2.imshow(edgesCheck)
 
-        #np.savetxt("imagedata.txt", data.reshape((3, -1)),fmt='%5.0f') # Without header
+        # np.savetxt("imagedata.txt", data.reshape((3, -1)),fmt='%5.0f') # Without header
 
         # plt.subplot(122),plt.imshow(edges,cmap = 'gray')
         # plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
         # plt.show()
-        # cv2.waitKey(0)
+
         # Wait for user input
-
-# Stop the webcam
-webcam.release()
-
-cv2.destroyAllWindows
+        # cv2.waitKey(0)
