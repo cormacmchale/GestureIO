a6e03941dfbb645f9c602b12c466a7ca144d24c4
unknown <cormacmchale1@gmail.com>
2020-02-05 16:46:36 +0000

    scalable way to build database implemented

--- webcam.py
+++ webcam.py
@@ -20,65 +20,44 @@ endBottom = 300
 color = (0, 255, 0)
 thickness = 1
 captureCounter = 0
+captureImages = False
 
 print('Recording...')
 
 while (running):
     check, frame = webcam.read()
-
     # cv2.rectangle() method is used to draw a rectangle on any image - gesture will be performed within the 
     # rectangle - For proccessing the image, the dataset will be built from the image captured within the rectangle
     newFrame = cv2.rectangle(frame, (startLeft - 5, startTop - 5), (endRight + 5, endBottom + 5), color, thickness)
-
     # Use Canny to find edges in the image
     edges = cv2.Canny(frame, firstThreshold, secondThreshold)
     # edgesTwo = frame[80:80,300:300]
-
     cv2.imshow("SignWriter", edges)
     key = cv2.waitKey(1)
 
-    # If user presses 'Q', program will capture image
-    if key == ord('c'):
-        # Keep track of number of images in .csv file
-        captureCounter += 1
-
-        # Save frame of webcam to an image
+    #build a large set of images
+    if(captureImages):
         cv2.imwrite('temp/originalImage.png', cv2.Canny(frame, 120, 120))
-
-        # Open the captured gesture
-        # No need to create multiple original images as the dataset won't be built from this image
         img = Image.open('temp/originalImage.png') 
         img = img.crop((startLeft, startTop, endRight, endBottom))
-        
-        # img.save('dataset/images/croppedImage.png')
-        # img.load()
-
         f = open('dataset/data/imagedata.csv', 'a')
-
         data = np.asarray(img, dtype='uint8')
-        #data[data > 0] = 1
-
-        # print(data.shape)
-        # print(data.size)
-        # print(data.dtype)
-
-        # f = open('dataset/data/imagedata.npz', 'w')
-
-        # np.savetxt(f, data, fmt='%s', delimiter='')
-        np.savetxt(f, data, fmt='%s')
-        
+        np.savetxt(f, data, fmt='%s')        
         f.close()
+        captureCounter = captureCounter + 1
+        #stop appending images
+        if(captureCounter==99):
+            captureImages = False
 
-        #checkArray = np.loadtxt('dataset/data/imagedata.csv', delimiter=',')
-        #checkArray.reshape(int (captureCounter), 220 / int (captureCounter), 220)
-        #print(captureCounter)
-        #print(checkArray.shape)
-        #print(checkArray.size)
-        # print(data.dtype)
-        # f.close();
+    # If user presses 'c', begin capture for 100 frames
+    if key == ord('c'):
+        if(captureImages):
+            captureImages = False
+        else:
+            captureImages = True
+
     if key == ord('q'):
-        print('Quitting program...')
-        
+        print('Quitting program...')        
         running = False
         break  
 
