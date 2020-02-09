0c389af79959a4c1c99687342e852932b71d6d9f
Kevin Niland <G00342279@gmit.ie>
2020-02-09 19:33:59 +0000

    Fixed issue with repo - project was pushed up as folder within project

--- webcam.py
+++ webcam.py
@@ -4,11 +4,6 @@ import keras as kr
 
 # Get model onto server for use
 from keras.models import load_model
-<<<<<<< HEAD
-import keyboard
-from matplotlib import pyplot as plt
-=======
->>>>>>> e5703c326e2c69113292087cd9c042704e3b9be7
 import numpy as np
 import PIL
 from PIL import Image 
@@ -62,34 +57,43 @@ while (running):
         np.savetxt(f, data, fmt='%s')        
         f.close()
         captureCounter = captureCounter + 1
+
         # Stop appending images
         if(captureCounter==200):
             captureImages = False
+
             # Reset
             print("finished")
             captureCounter = 0
     
     #analyse every x amount of frames
     frameCounter = frameCounter + 1
+
     if(frameCounter %  55 == 0):
         cv2.imwrite('temp/originalImage.png', edgesFiltered)
+
         img = Image.open('temp/originalImage.png') 
         img = img.crop((startLeft, startTop, endRight, endBottom))
-        data = np.asarray(img, dtype='uint8').reshape(1,48400)
+
+        data = np.asarray(img, dtype='uint8').reshape(1, 48400)
+
         inputVector = data.copy()
-        #img = np.array(Image.open(filename))
-        #inputVector.setflags(write=1)
+
+        # img = np.array(Image.open(filename))
+        # inputVector.setflags(write=1)
+
         inputVector[inputVector > 0] = 1
         inputVector[inputVector < 1] = 0
         prediction = abstractPredic(data, numberRecoq)
+
         f = open('predictions/checkPrediction.txt', 'a')
         # Perfrom action here for gesture recognition
         if(prediction == 0):
             f.write("No Gesture" + "\n")
         elif (prediction == 1):
             f.write("Open Hand" + "\n")
-            #webbrowser.open('https://learnonline.gmit.ie/', new=2)
-            #removed for testing
+            # Removed for testing
+            # webbrowser.open('https://learnonline.gmit.ie/', new=2)
         elif(prediction == 2):
             f.write("Peace Sign" + "\n")
         elif(prediction == 3):
@@ -98,43 +102,51 @@ while (running):
             f.write("Garbage" + "\n")
         else:
             f.write("ERROR: You shouldn't see this")
+
         f.close()
         frameCounter = 0
       
-    #user interaction
+    # User interaction
     # If user presses 'c', begin capture for 100 frames
     if key == ord('c'):
         if(captureImages):
             captureImages = False
         else:
             captureImages = True
+
     if key == ord('p'):
         cv2.imwrite('temp/originalImage.png', edgesFiltered)
+
         img = Image.open('temp/originalImage.png') 
         img = img.crop((startLeft, startTop, endRight, endBottom))
-        data = np.asarray(img, dtype='uint8').reshape(1,48400)
+
+        data = np.asarray(img, dtype='uint8').reshape(1, 48400)
+
         # data[data > 0] = 1
         # data[data < 1] = 0
+
         prediction = abstractPredic(data, numberRecoq)
         f = open('predictions/checkPrediction.txt', 'a')
+
         # Perfrom action here for gesture recognition
         if(prediction == 0):
             f.write("No Gesture" + "\n")
         elif (prediction == 1):
             f.write("Open" + "\n")
-            #webbrowser.open('https://www.google.com/', new=2)
+            # webbrowser.open('https://www.google.com/', new=2)
         elif(prediction == 2):
             f.write("Peace Sign" + "\n")
-            keyboard.write("hello friend")
         else:
             f.write("ERROR: You shouldn't see this")
         f.close()
+
     if key == ord('q'):
         print('Quitting program...')  
+
         running = False
         break  
 
-    #organising the edges detection
+    # Organising the edges detection
     if key == ord('y'):
         firstThreshold = firstThreshold + 20
         print ("first Threshold "+str(firstThreshold))
@@ -144,9 +156,11 @@ while (running):
         secondThreshold = secondThreshold + 20
         print ("second Threshold "+str(secondThreshold))
     if key == ord('j'):
-        secondThreshold = secondThreshold - 20       
+        secondThreshold = secondThreshold - 20 
+
 # Stop the webcam
 webcam.release()
+
 cv2.destroyAllWindows
 
 ''' Unused code '''
