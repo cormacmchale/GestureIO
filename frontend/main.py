8795002f5c4526bb511be465c07686707da75e9d
Kevin Niland <G00342279@gmit.ie>
2020-02-13 13:00:39 +0000

    More work on frontend

--- frontend/main.py
+++ frontend/main.py
@@ -1,8 +1,20 @@
-import tkinter
+import tkinter as tk
+from tkinter import *
 
-window = tkinter.Tk()
+from PIL import ImageTk, Image
+
+path = 'gestureImages/openhand.png'
+
+window = tk.Tk()
 
 window.title("SignWriter")
 window.geometry("700x500")
+window.configure(bg="white")
+
+photo = PhotoImage(file=path)
+
+w = Label(window, image=photo)
+
+w.grid(row=3, column=3)
 
 window.mainloop()
