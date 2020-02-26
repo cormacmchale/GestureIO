d9a65a1a2ec2c232f41a9410493caf9d65d539dc
Kevin Niland <G00342279@gmit.ie>
2020-02-26 20:31:39 +0000

    Added rectangle to indicate where user should perform a gesture

--- frontend/main_gui.py
+++ frontend/main_gui.py
@@ -3,14 +3,20 @@ from tkinter import *
 import cv2
 from PIL import Image, ImageTk
 
-w, h = 800, 600
+# Variables
+width, height = 450, 450
+startLeft, startTop = 80, 80
+endRight, endBottom = 300, 300
+color = (0, 0, 0)
+thickness = 1
+
 webcam = cv2.VideoCapture(0)
 
 openhand = 'gestureImages/openhand.png'
 fist = 'gestureImages/fist.png'
 
-webcam.set(cv2.CAP_PROP_FRAME_WIDTH, w)
-webcam.set(cv2.CAP_PROP_FRAME_HEIGHT, h)
+webcam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+webcam.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
 root = tk.Tk()
 root.title("SignWriter")
@@ -24,8 +30,8 @@ photoTwo = PhotoImage(file=fist)
 w1 = Label(root, image=photoOne)
 w2 = Label(root, image=photoTwo)
 
-w1.grid(row=3, column=3)
-w2.grid(row=3, column=2)
+w1.grid(row=1, column=2)
+w2.grid(row=2, column=2)
 
 main = tk.Label(root)
 main.grid(row=1, column=1)
@@ -33,6 +39,7 @@ main.grid(row=1, column=1)
 def showWebcam():
     _, frame = webcam.read()
     frame = cv2.flip(frame, 1)
+    frame = cv2.rectangle(frame, (startLeft - 5, startTop - 5), (endRight + 5, endBottom + 5), color, thickness)
     cv2Image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
 
     img = Image.fromarray(cv2Image)
